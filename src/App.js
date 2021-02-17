8e0de4cd7420ddfb039f6428ffd96a4fb3991afa
Max Ivanov <maximus-dr@mail.ru>
2021-02-17 10:11:54 +0300

    Basic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Navbar from './app/Navbar';
 
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Switch>
+          <Route exact path="/" render={() => (
+            <section>
+              <h2>Welcome to Redux Social App</h2>
+            </section>
+          )}>
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
---
+++ src/app/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav>
+      <section>
+        <h1>Redux social app example</h1>
+
+        <div className="navContent">
+          <div className="navLinks"></div>
+        </div>
+      </section>
+    </nav>
+  )
+}
