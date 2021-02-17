ebeda957805a7a24b724c7cc064242530dcb6153
Max Ivanov <maximus-dr@mail.ru>
2021-02-17 13:50:53 +0300

    Added AJAX request client in api/client.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-
-
-const store = createStore(() => ({}));
+import store from './app/store';
 
 
 ReactDOM.render(
