8265f9260631ec3bb96675182c84141ebbfcb212
Max Ivanov <maximus-dr@mail.ru>
2021-02-18 22:36:42 +0300

    Modified initialState in postsSlice

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,37 +1,43 @@
-import { sub } from "date-fns";
+// import { sub } from "date-fns";
+
+// const dummyPosts = [
+//   { 
+//     id: '1', 
+//     title: 'First post!', 
+//     content: 'Hello', 
+//     user: '0', 
+//     date: sub(new Date(), {minutes: 10}).toISOString(),
+//     reactions: {
+//       thumbsUp: 0,
+//       hooray: 0,
+//       heart: 0,
+//       rocket: 0,
+//       eyes: 0
+//     } 
+//   },
+//   { 
+//     id: '2', 
+//     title: 'Second post!', 
+//     content: 'More text', 
+//     user: '1', 
+//     date: sub(new Date(), {minutes: 5}).toISOString(),
+//     reactions: {
+//       thumbsUp: 0,
+//       hooray: 0,
+//       heart: 0,
+//       rocket: 0,
+//       eyes: 0
+//     } 
+//   }
+// ];
 
 const { createSlice, nanoid } = require("@reduxjs/toolkit");
 
-const initialState = [
-  { 
-    id: '1', 
-    title: 'First post!', 
-    content: 'Hello', 
-    user: '0', 
-    date: sub(new Date(), {minutes: 10}).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      hooray: 0,
-      heart: 0,
-      rocket: 0,
-      eyes: 0
-    } 
-  },
-  { 
-    id: '2', 
-    title: 'Second post!', 
-    content: 'More text', 
-    user: '1', 
-    date: sub(new Date(), {minutes: 5}).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      hooray: 0,
-      heart: 0,
-      rocket: 0,
-      eyes: 0
-    } 
-  }
-];
+const initialState = {
+  posts: [],
+  status: 'idle',
+  error: null
+};
 
 const postSlice = createSlice({
   name: 'posts',
@@ -40,7 +46,7 @@ const postSlice = createSlice({
 
     postAdded: {
       reducer(state, action) {
-        state.push(action.payload);
+        state.posts.push(action.payload);
       },
       prepare(title, content, userId) {
         return {
@@ -49,7 +55,14 @@ const postSlice = createSlice({
             title,
             content,
             user: userId,
-            date: new Date().toISOString()
+            date: new Date().toISOString(),
+            reactions: {
+              thumbsUp: 0,
+              hooray: 0,
+              heart: 0,
+              rocket: 0,
+              eyes: 0
+            }
           }
         }
       }
@@ -57,7 +70,7 @@ const postSlice = createSlice({
 
     postUpdated(state, action) {
       const { id, title, content } = action.payload;
-      const existingPost = state.find(post => post.id === id);
+      const existingPost = state.posts.find(post => post.id === id);
 
       if (existingPost) {
         existingPost.title = title;
@@ -67,7 +80,7 @@ const postSlice = createSlice({
 
     reactionAdded(state, action) {
       const { postId, reaction } = action.payload;
-      const existingPost = state.find(post => post.id === postId);
+      const existingPost = state.posts.find(post => post.id === postId);
 
       if (existingPost) {
         existingPost.reactions[reaction]++;
@@ -76,9 +89,9 @@ const postSlice = createSlice({
   }
 });
 
-export const selectAllPosts = (state) => state.posts;
+export const selectAllPosts = (state) => state.posts.posts;
 export const selectPostById = (state, postId) =>
-  state.posts.find(post => post.id === postId);
+  state.posts.posts.find(post => post.id === postId);
 
 // automaticly generated Action Creators
 export const { postAdded, postUpdated, reactionAdded } = postSlice.actions;
