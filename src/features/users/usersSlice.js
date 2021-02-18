1da54e335652c85682bac0bf89bd6a1d877e1058
Max Ivanov <maximus-dr@mail.ru>
2021-02-18 14:52:25 +0300

    Implemented usersSlice

---
+++ src/features/users/usersSlice.js
@@ -0,0 +1,15 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const initialState = [
+  {id: '0', name: 'Tatsjana Jerkins'},
+  {id: '1', name: 'Kevin Grant'},
+  {id: '2', name: 'Madison Price'}
+];
+
+const usersSlice = createSlice({
+  name: 'users', 
+  initialState,
+  reducers: {}
+});
+
+export default usersSlice;
